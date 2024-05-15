a1329874c4dc29755d52cb5e7c15264521665725
momogoyo <metheduxk@gmail.com>
2024-05-15 23:12:37 +0900

    💄 style 적용

--- src/Cinnamonroll.ts
+++ src/Cinnamonroll.ts
@@ -1,6 +1,11 @@
 import type { Configs } from './type'
 
+// default configs
+
+
 function Cinnamonroll (element: HTMLElement, configs: Configs) {
+  const scroller = element.querySelector('#scroller')
+
   const initialize = () => {
     console.log('Initializing', element, configs)
   }
